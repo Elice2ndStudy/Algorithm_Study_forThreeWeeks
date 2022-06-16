d3a8a16f7ab06a50a3c5a62c1b7544dc057647fe
jpark <tmdeksdl@gmail.com>
2022-06-17 00:28:55 +0900

    BOJ_1106 / 호텔 / 골드5

--- seungjun/BOJ_1106.py
+++ seungjun/BOJ_1106.py
@@ -1,3 +1,25 @@
 # 호텔
 # https://www.acmicpc.net/problem/1106
+import sys
+input = sys.stdin.readline
 
+c, n = map(int, input().split())
+arr = []
+dp = [0] + [int(1e9)] * (c+100) 
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+    # [cost, customer]
+
+arr_sort = sorted(arr, key = lambda x: x[0]) 
+# x[0]인 cost를 기준으로 오름차순 정리를 해준다
+# 그냥 sorted을 해주어도 되지만 lambda sorting이 더 범용적이라 채택
+
+for cost, customer in arr_sort:
+    for i in range(customer, c+100):
+        dp[i] = min(dp[i], dp[i-customer] + cost)
+
+
+
+print(dp)
+
+print(min(dp[c:]))
